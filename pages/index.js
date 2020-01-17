ec47325bf79be628fa206c7fa60c682d621cceae
Duviel Garcia <duviel7@gmail.com>
2020-01-16 21:27:41 -0500

    Consulta a la API de noticias, Se muestran los resultados en dos componentes, Noticias que lista las noticias y Noticia que muestra la información de cada una de las noticias

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import MasterPage from '../components/Master';
 import fetch from 'isomorphic-unfetch';
 
 import Precio from '../components/Precio';
+import Noticias from '../components/Noticias';
 
 const Index = (props) => (
     <MasterPage>
@@ -13,6 +14,9 @@ const Index = (props) => (
 
             <div className="col-md-8">
                 <h2>Noticias Sobre Bitcoin</h2>
+                <Noticias
+                    noticias={props.noticias}
+                />
             </div>
             <div className="col-md-4">
                 Proximos Eventos
@@ -23,11 +27,14 @@ const Index = (props) => (
 
 Index.getInitialProps = async () => {
     const precio = await fetch('https://api.coinmarketcap.com/v2/ticker/1/');
-
+    const noticias = await fetch('https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-17&sortBy=publishedAt&apiKey=705a3b8215df4a0188444fc489fa0ea5&language=es');
+    
     const resPrecio = await precio.json();
+    const resNoticias = await noticias.json();
 
     return {
-        precioBitcoin: resPrecio.data.quotes.USD
+        precioBitcoin : resPrecio.data.quotes.USD,
+        noticias : resNoticias.articles
     }
 }
 export default Index;
